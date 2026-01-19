eab30251a149a11d2f962ad98ca60769c27db9b2
ThaiVu <thaivq94@gmail.com>
2026-01-19 08:41:58 +0700

    [UPDATE]-app_config.h combine with drives.h

--- prj/drivers.h
+++ prj/drivers.h
@@ -32,7 +32,7 @@
 // * TBS project define pinout
 // * */
 #ifndef MASTER_CORE
-#define TBS_COUNTER_DEVICE 1 			//0: Counter, 1: PowerMeter
+#define TBS_COUNTER_DEVICE 0 			//0: Counter, 1: PowerMeter
 #if TBS_COUNTER_DEVICE
 #define COUNTER_DEVICE
 #else
@@ -69,5 +69,30 @@
 		#define HSPI_CS_POWER_METER_STPM2		GPIO_PE5
 		#define HSPI_CS_POWER_METER_STPM3		GPIO_PE4
 	#endif
+#endif
+
+/////////////////////////////////////// PRINT DEBUG INFO ///////////////////////////////////////
+#if (UART_PRINT_DEBUG_ENABLE)
+//the baud rate should not bigger than 115200 when MCU clock is 16M)
+//the baud rate should not bigger than 1000000 when MCU clock is 24M)
+#ifdef POWER_METER_DEVICE
+#define PRINT_BAUD_RATE             		115200
+#define DEBUG_INFO_TX_PIN           		GPIO_PE0
+#define PULL_WAKEUP_SRC_PE0         		PM_PIN_PULLUP_10K
+#define PE0_OUTPUT_ENABLE         			1
+#define PE0_DATA_OUT                     	1 //must
+#else
+#define PRINT_BAUD_RATE             		115200
+#define DEBUG_INFO_TX_PIN           		GPIO_PA7
+#define PULL_WAKEUP_SRC_PA7         		PM_PIN_PULLUP_10K
+#define PA7_OUTPUT_ENABLE         			1
+#define PA7_DATA_OUT                     	1 //must
+#endif
+#define DEBUG_TX_PIN_INIT()					do{	\
+												gpio_function_en(DEBUG_INFO_TX_PIN);								\
+												gpio_set_output(DEBUG_INFO_TX_PIN, 1);								\
+												gpio_set_up_down_res(DEBUG_INFO_TX_PIN, GPIO_PIN_PULLUP_1M);		\
+												gpio_set_high_level(DEBUG_INFO_TX_PIN);								\
+											}while(0)
 #endif
 
